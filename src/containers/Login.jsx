6a904b6419717e0557667d2b9eb1ea7a68238e08
Rabm1504 <rennyangel2@gmail.com>
2023-01-19 14:33:02 -0500

    2

---
+++ src/containers/Login.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/Login.scss';
+
+const Login = () => {
+	return (
+		<div class="box">
+            <h2>Login</h2>
+            <form>
+            <div className="inputBox">
+                <input type="email" name="email" required onkeyup="this.setAttribute('value', this.value);" value=""/>
+                <label>Username</label>
+            </div>
+            <div className="inputBox">
+                <input type="password" name="password" required value="" onkeyup="this.setAttribute('value', this.value);" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"/>
+                <label>Password</label>
+            </div>
+            <input type="submit" name="sign-in" value="Sign In"/>
+            </form>
+        </div>
+	);
+}
+
+export default Login;
